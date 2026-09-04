db80a2b600e1b38b098fcd65124c662713d0c1bf
CedricA <cedric.j.aoun@gmail.com>
2026-09-04 16:32:25 +0200

    Use proper CRA env variable name

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = import.meta.env.NODE_API_URL || 'http://localhost:5000/api';
+const URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Comment gérer différentes URLs pour le développement, la pré-production et la production ?
 const api = axios.create({
