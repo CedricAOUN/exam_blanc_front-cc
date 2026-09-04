4075ce8b4ee3cb708d22ccca3dd0823dd7a1c3ae
CedricA <cedric.j.aoun@gmail.com>
2026-09-04 16:14:02 +0200

    add env for API URL

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const URL = import.meta.env.NODE_API_URL || 'http://localhost:5000/api';
+
 // Comment gérer différentes URLs pour le développement, la pré-production et la production ?
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: URL,
 });
 
 export default api;
